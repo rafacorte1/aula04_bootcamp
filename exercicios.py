895d96d57850108798310b1235d846956dadd6e7
Rafael Corte <rafcorte@gmail.com>
2026-01-22 17:16:05 -0300

    exercicio 1

--- exercicios.py
+++ exercicios.py
@@ -34,3 +34,11 @@ lista_de_livros = {
 
 print(lista_de_livros['livro1']['Título'])
 # %%
+
+# Crie uma lista com os números de 1 a 10 e use um loop para imprimir cada número elevado ao quadrado.
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for i in lista:
+    print(i ** 2)
+# %%
